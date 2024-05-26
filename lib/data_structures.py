2e876e7c897493bede84bc0e8fdd2d6484aebce9
Gideon Macharia <kamaugedion95@gmail.com>
2024-05-27 02:20:24 +0300

    final commit

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -20,34 +20,16 @@ def get_names(spicy_foods):
     return [food["name"] for food in spicy_foods]
 
 def get_spiciest_foods(spicy_foods):
-    return [food["heat_level"] for food in spicy_foods if food["heat_level"]>5]
-#get_spiciest_foods(['food'])
+    spiciest_foods = [food for food in spicy_foods if food['heat_level'] > 5]
+    return spiciest_foods
+
 
 def print_spicy_foods(spicy_foods):
     for food in spicy_foods:
         heat_level_emojis = "🌶" * food["heat_level"]
         print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level_emojis}")
 
-# Example usage:
-spicy_foods = [
-    {
-        "name": "Green Curry",
-        "cuisine": "Thai",
-        "heat_level": 9,
-    },
-    {
-        "name": "Buffalo Wings",
-        "cuisine": "American",
-        "heat_level": 3,
-    },
-    {
-        "name": "Mapo Tofu",
-        "cuisine": "Sichuan",
-        "heat_level": 6,
-    },
-]
 
-print_spicy_foods(spicy_foods)
 
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
@@ -55,8 +37,8 @@ def get_spicy_food_by_cuisine(spicy_foods, cuisine):
     return matching_foods[0] if matching_foods else None
 
 def print_spiciest_foods(spicy_foods):
-   spiciest_foods = get_spiciest_foods(spicy_foods)
-   print_spiciest_foods(spiciest_foods)
+    spiciest_foods = get_spiciest_foods(spicy_foods)
+    print_spicy_foods(spiciest_foods)
 
 def get_average_heat_level(spicy_foods):
     total_heat = sum(food["heat_level"]for food in spicy_foods)
@@ -68,32 +50,4 @@ def get_average_heat_level(spicy_foods):
 def create_spicy_food(spicy_foods, new_spicy_food):
     spicy_foods.append(new_spicy_food)
     return spicy_foods
-
-# Example usage:
-spicy_foods = [
-    {
-        "name": "Green Curry",
-        "cuisine": "Thai",
-        "heat_level": 9,
-    },
-    {
-        "name": "Buffalo Wings",
-        "cuisine": "American",
-        "heat_level": 3,
-    },
-    {
-        "name": "Mapo Tofu",
-        "cuisine": "Sichuan",
-        "heat_level": 6,
-    },
-]
-
-new_spicy_food = {
-    "name": "Kimchi",
-    "cuisine": "Korean",
-    "heat_level": 7
-}
-
-updated_spicy_foods = create_spicy_food(spicy_foods, new_spicy_food)
-print(updated_spicy_foods)
 
